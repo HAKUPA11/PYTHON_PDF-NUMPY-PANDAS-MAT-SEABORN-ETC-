f7c60937a46e39d490877b233d2a0ab5a9cf4fa7
HAKUPA11 <ishnarsh1119@gmail.com>
2025-12-02 16:25:23 +0530

    initial commit

---
+++ hakupa1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+df=pd.read_csv("ecommerce_customers_unit1.csv")
+df.shape
+df.head()
